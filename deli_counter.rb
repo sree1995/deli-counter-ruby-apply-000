eb2491ca8b8b0b65883c17e7f9721ae5b1505a69
Sree <harsha6raju@yahoo.com>
2018-05-03 14:40:49 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,34 +1,12 @@
-katz_deli = []
+<<<<<<< HEAD
+=======
+# Write your code here.
+>>>>>>> 217cc25afad3acbf1463b7385debfeb6caa6627c
 
-# line status
-def line(customers)
-  line = customers.map.with_index {|x, index| "#{index + 1}. #{x}"}.join(' ')
-  if customers.empty?
-    puts "The line is currently empty."
-  else
-    puts "The line is currently: #{line}"
-  end
-end
-
-# register the customer 
-def take_a_number(current_line, name)
- if current_line.empty?
-   current_line.push(name)
-   puts "Welcome, #{name}. You are number #{current_line.index(name) + 1} in line."
- elsif current_line.empty? != true
-    current_line.push(name)
-    puts "Welcome, #{name}. You are number #{current_line.index(name) + 1} in line."
- end
- 
- # currently serving..customer
- def now_serving(current_line)
-   if current_line.empty?
-     puts "There is nobody waiting to be served!"
-   else
-     puts "Currently serving #{current_line[0]}."
-     current_line.shift
-   end
- end
  
   
+<<<<<<< HEAD
 end
+=======
+end
+>>>>>>> 217cc25afad3acbf1463b7385debfeb6caa6627c
